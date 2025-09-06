e4f626eb0649bc83ddb4ad64feee3bc97140bdb3
Julian Torres <julian420torres@users.noreply.github.com>
2025-09-05 21:07:58 -0500

    Actualiza public/js/scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,42 +1,36 @@
 /*!
-    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2022 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
+ * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+ */
+//
 // Scripts
-// 
+//
 
-
-window.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener("DOMContentLoaded", (event) => {
     // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    const sidebarToggle = document.body.querySelector("#sidebarToggle");
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
         //     document.body.classList.toggle('sb-sidenav-toggled');
         // }
-        sidebarToggle.addEventListener('click', event => {
+        sidebarToggle.addEventListener("click", (event) => {
             event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            document.body.classList.toggle("sb-sidenav-toggled");
+            localStorage.setItem(
+                "sb|sidebar-toggle",
+                document.body.classList.contains("sb-sidenav-toggled")
+            );
         });
     }
-
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const botonAgregar = document.getElementById("btn_agregar");
-    
-    if (botonAgregar) {
-        if (typeof agregarProducto === "function") {
-            botonAgregar.addEventListener("click", agregarProducto);
-            console.log("✅ Botón 'Agregar' encontrado y evento asignado.");
-        } else {
-            console.warn("⚠️ La función agregarProducto no está definida aún.");
-        }
-    } else {
-        console.log("❌ No se encontró el botón 'Agregar'");
+    // Botón de AGREGAR para VENTAS
+    const botonAgregarCompra = document.getElementById("btn_agregar_venta");
+    if (botonAgregarCompra && typeof agregarProducto === "function") {
+        botonAgregarCompra.addEventListener("click", agregarProducto);
+        console.log("✅ Botón 'Agregar Compra' encontrado y evento asignado.");
     }
 });
